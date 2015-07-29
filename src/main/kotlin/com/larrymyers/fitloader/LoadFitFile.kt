221e48408993e1a66a4830e046cdcb835df08c98
Larry Myers <larry@larrymyers.com>
2015-07-28 21:46:29 -0500

    Stub out basic CLI.

--- src/main/kotlin/com/larrymyers/fitloader/LoadFitFile.kt
+++ src/main/kotlin/com/larrymyers/fitloader/LoadFitFile.kt
@@ -1,5 +1,36 @@
 package com.larrymyers.fitloader
 
+import net.sourceforge.argparse4j.ArgumentParsers
+import net.sourceforge.argparse4j.inf.ArgumentParser
+import net.sourceforge.argparse4j.inf.ArgumentParserException
+import net.sourceforge.argparse4j.inf.Namespace
+import kotlin.platform.platformStatic
+
 class LoadFitFile() {
+
+    companion object {
+        platformStatic fun main(args: Array<String>) {
+            val parser = ArgumentParsers.newArgumentParser("LoadFitFile")
+                .defaultHelp(true)
+                .description("Load a .fit file from disk.")
+
+            parser.addArgument("file").nargs("*")
+                .help("path to fit file")
+
+
+            val ns: Namespace
+
+            try {
+                ns = parser.parseArgs(args)
+            } catch (ex: ArgumentParserException) {
+                parser.handleError(ex)
+                return System.exit(1)
+            }
+
+            for (filename in ns.getList<String>("file")) {
+                println(filename)
+            }
+        }
+    }
 
 }
